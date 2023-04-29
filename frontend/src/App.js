be8979ecc9aff9c88f4e840e2eb18c670c2a3448
Salamata4255 <sb4255@drexel.edu>
2023-04-28 21:40:40 -0400

    frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import {Routes, Route }from 'react-router-dom'
+import Homepage from './Homepage/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/home" element={<Homepage />}></Route>
+      </Routes>
     </div>
   );
 }
