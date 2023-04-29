ce3df2e0b06d4ff1732da4877bee73ef874a82b4
Salamata4255 <sb4255@drexel.edu>
2023-04-29 15:06:33 -0400

    symptoms done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route }from 'react-router-dom'
 import Homepage from './Homepage/Home';
 import './App.css';
 import PersonalInfo from './PersonalInfo/PersonalInfo';
+import Symptoms from './Symptoms/Symptoms';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/home" element={<Homepage />}></Route>
         <Route path="/info" element={<PersonalInfo />}></Route>
+        <Route path="/symptoms" element={<Symptoms />}></Route>
       </Routes>
     </div>
   );
