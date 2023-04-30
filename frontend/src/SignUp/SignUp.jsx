0652b03c9b6a8e5e204bb5eb5b61e72e3585d0a0
Raunaq Malhotra <raunaqmalhotra02@gmail.com>
2023-04-29 20:10:12 -0400

    fix errors

--- frontend/src/SignUp/SignUp.jsx
+++ frontend/src/SignUp/SignUp.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import './SignUp.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaHome } from 'react-icons/fa';
 
 const SignUp = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
